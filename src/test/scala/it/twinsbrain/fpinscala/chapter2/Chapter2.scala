f31cccda45a467f83afe7c6e5918e52f9e75d960
Paolo Sciarra <itbhps@gmail.com>
2018-10-15 19:02:20 +0200

    tail recursive fibonacci

--- src/test/scala/it/twinsbrain/fpinscala/chapter2/Chapter2.scala
+++ src/test/scala/it/twinsbrain/fpinscala/chapter2/Chapter2.scala
@@ -2,8 +2,27 @@ package it.twinsbrain.fpinscala.chapter2
 
 import org.scalatest._
 
+import scala.annotation.tailrec
+
 class Chapter2Test extends FunSuite with Matchers {
-  test("first") {
-    assert(true == true)
+  test("fibonacci") {
+    fib(0) should be(0)
+    fib(1) should be(1)
+    fib(2) should be(1)
+    fib(3) should be(2)
+    fib(4) should be(3)
+    fib(5) should be(5)
+    fib(6) should be(8)
+  }
+
+  def fib(n: Int): Int = {
+
+    @tailrec
+    def loop(last: Int, curr: Int, count: Int): Int = {
+      if (count == n) curr
+      else loop(curr, curr + last, count + 1)
+    }
+
+    loop(1, 0, 0)
   }
 }
