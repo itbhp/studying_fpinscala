14bcc43f86da0a2cbe48a32b5f3dc7b7354ccaef
Paolo Sciarra <itbhps@gmail.com>
2018-10-15 20:38:01 +0200

    uncurrying

--- src/test/scala/it/twinsbrain/fpinscala/chapter2/CurryingTest.scala
+++ src/test/scala/it/twinsbrain/fpinscala/chapter2/CurryingTest.scala
@@ -12,4 +12,12 @@ class CurryingTest extends FunSuite with Matchers{
 
   def curry[A,B,C](f: (A, B) => C): A => (B => C) = a => b => f(a, b)
 
+  test("uncurry"){
+    val productCurried: (Int) => (Int) => Int = a => b => a * b
+
+    productCurried(2)(3) shouldEqual(uncurry(productCurried)(2,3))
+  }
+
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C = (a, b) => f(a)(b)
+
 }
