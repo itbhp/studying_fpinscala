b3b597f97d7c22ca857f8e3e9405f10aedbb1eb3
Paolo Sciarra <itbhps@gmail.com>
2018-10-15 20:55:51 +0200

    code refactoring

--- src/test/scala/it/twinsbrain/fpinscala/chapter2/CompositionTest.scala
+++ src/test/scala/it/twinsbrain/fpinscala/chapter2/CompositionTest.scala
@@ -10,7 +10,7 @@ class CompositionTest extends FunSuite with Matchers{
     val productBy3 = (a:Int) => a * 3
     val productBy6 = (a:Int) => a * 6
 
-    my_compose(productBy2, productBy3) (4) shouldEqual(productBy6(4))
+    my_compose(productBy2, productBy3) (4) shouldEqual productBy6(4)
   }
 
   test("compose functions different domains"){
@@ -19,7 +19,7 @@ class CompositionTest extends FunSuite with Matchers{
     val inverse: Int => Double = (a:Int) => 1.0 / a
     val inverseAsString = (a:Int) => (1.0 / a).toString
 
-    my_compose(doubleToStr, inverse) (4) shouldEqual(inverseAsString(4))
+    my_compose(doubleToStr, inverse) (4) shouldEqual inverseAsString(4)
   }
 
   test("my compose vs scala compose"){
@@ -27,7 +27,7 @@ class CompositionTest extends FunSuite with Matchers{
     val doubleToStr: Double => String = (a: Double) => a.toString
     val inverse: Int => Double = (a:Int) => 1.0 / a
 
-    my_compose(doubleToStr, inverse) (4) shouldEqual((doubleToStr compose inverse)(4))
+    my_compose(doubleToStr, inverse) (4) shouldEqual (doubleToStr compose inverse)(4)
   }
 
   test("my compose vs scala andThen"){
@@ -35,7 +35,7 @@ class CompositionTest extends FunSuite with Matchers{
     val doubleToStr: Double => String = (a: Double) => a.toString
     val inverse: Int => Double = (a:Int) => 1.0 / a
 
-    my_compose(doubleToStr, inverse) (4) shouldEqual((inverse andThen  doubleToStr)(4))
+    my_compose(doubleToStr, inverse) (4) shouldEqual (inverse andThen  doubleToStr)(4)
   }
 
   def my_compose[A,B,C](f: B => C, g: A => B): A => C = a => f(g(a))
--- src/test/scala/it/twinsbrain/fpinscala/chapter2/CurryingTest.scala
+++ src/test/scala/it/twinsbrain/fpinscala/chapter2/CurryingTest.scala
@@ -7,7 +7,7 @@ class CurryingTest extends FunSuite with Matchers{
   test("curry"){
     val product: (Int,Int) => Int = (a,b) => a * b
 
-    curry(product)(2)(3) shouldEqual(product(2,3))
+    curry(product)(2)(3) shouldEqual product(2,3)
   }
 
   def curry[A,B,C](f: (A, B) => C): A => (B => C) = a => b => f(a, b)
@@ -15,7 +15,7 @@ class CurryingTest extends FunSuite with Matchers{
   test("uncurry"){
     val productCurried: (Int) => (Int) => Int = a => b => a * b
 
-    productCurried(2)(3) shouldEqual(uncurry(productCurried)(2,3))
+    productCurried(2)(3) shouldEqual uncurry(productCurried)(2,3)
   }
 
   def uncurry[A,B,C](f: A => B => C): (A, B) => C = (a, b) => f(a)(b)
