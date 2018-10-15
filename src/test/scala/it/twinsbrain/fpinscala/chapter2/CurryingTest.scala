a9533cb94772c2ed98c7d19fe5637d2fafe72d91
Paolo Sciarra <itbhps@gmail.com>
2018-10-15 20:35:21 +0200

    currying

---
+++ src/test/scala/it/twinsbrain/fpinscala/chapter2/CurryingTest.scala
@@ -0,0 +1,15 @@
+package it.twinsbrain.fpinscala.chapter2
+
+import org.scalatest._
+
+class CurryingTest extends FunSuite with Matchers{
+
+  test("curry"){
+    val product: (Int,Int) => Int = (a,b) => a * b
+
+    curry(product)(2)(3) shouldEqual(product(2,3))
+  }
+
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) = a => b => f(a, b)
+
+}
