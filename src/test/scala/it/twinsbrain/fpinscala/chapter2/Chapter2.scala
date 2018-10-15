c6ca11275940aec299b426f36aa2e35149afcd2a
Paolo Sciarra <itbhps@gmail.com>
2018-10-15 19:40:13 +0200

    implemented a polymorphic isSorted over a collection given an ordering function

--- src/test/scala/it/twinsbrain/fpinscala/chapter2/Chapter2.scala
+++ src/test/scala/it/twinsbrain/fpinscala/chapter2/Chapter2.scala
@@ -5,7 +5,28 @@ import org.scalatest._
 import scala.annotation.tailrec
 
 class Chapter2Test extends FunSuite with Matchers {
-  test("fibonacci") {
+
+  test("isSorted") {
+    val fromLesserToGreater: (Int, Int) => Boolean = (prev: Int, curr: Int) => prev < curr
+    withClue("Array(2,3,1)") {
+      isSorted(Array(2,3,1), fromLesserToGreater) should be(false)
+    }
+    withClue("Array(1,2,3)") {
+      isSorted(Array(1,2,3), fromLesserToGreater) should be(true)
+    }
+  }
+
+  def isSorted[A](collection: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    def loop(currentIndex: Int):Boolean = {
+      if(currentIndex > collection.length - 1) true
+      else if(!ordered(collection(currentIndex - 1), collection(currentIndex))) false
+      else loop(currentIndex + 1)
+    }
+    if(collection.length < 2) true
+    else loop( 1)
+  }
+
+  ignore("fibonacci") {
     withClue("fib(0) =") { fib(0) should be(0) }
     withClue("fib(1) =") { fib(3) should be(2) }
     withClue("fib(2) =") { fib(1) should be(1) }
