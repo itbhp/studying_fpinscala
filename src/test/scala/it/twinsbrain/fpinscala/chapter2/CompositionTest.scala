497c4e630bcfdc95cd1ac70ad140ffaf7e0afb2b
Paolo Sciarra <itbhps@gmail.com>
2018-10-15 20:53:38 +0200

    composition

--- src/test/scala/it/twinsbrain/fpinscala/chapter2/CompositionTest.scala
+++ src/test/scala/it/twinsbrain/fpinscala/chapter2/CompositionTest.scala
@@ -10,7 +10,7 @@ class CompositionTest extends FunSuite with Matchers{
     val productBy3 = (a:Int) => a * 3
     val productBy6 = (a:Int) => a * 6
 
-    compose(productBy2, productBy3) (4) shouldEqual(productBy6(4))
+    my_compose(productBy2, productBy3) (4) shouldEqual(productBy6(4))
   }
 
   test("compose functions different domains"){
@@ -19,9 +19,18 @@ class CompositionTest extends FunSuite with Matchers{
     val inverse: Int => Double = (a:Int) => 1.0 / a
     val inverseAsString = (a:Int) => (1.0 / a).toString
 
-    compose(doubleToStr, inverse) (4) shouldEqual(inverseAsString(4))
+    my_compose(doubleToStr, inverse) (4) shouldEqual(inverseAsString(4))
   }
 
-  def compose[A,B,C](f: B => C, g: A => B): A => C = a => f(g(a))
+  test("my compose vs scala compose"){
+
+    val doubleToStr: Double => String = (a: Double) => a.toString
+    val inverse: Int => Double = (a:Int) => 1.0 / a
+//    val inverseAsString = (a:Int) => (1.0 / a).toString
+
+    my_compose(doubleToStr, inverse) (4) shouldEqual((doubleToStr compose inverse)(4))
+  }
+
+  def my_compose[A,B,C](f: B => C, g: A => B): A => C = a => f(g(a))
 
 }
