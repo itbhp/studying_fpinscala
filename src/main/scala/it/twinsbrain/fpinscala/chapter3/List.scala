a92d79f0e6fdc42bf83bf73255393881d6c633c4
Paolo Sciarra <itbhps@gmail.com>
2018-10-18 13:44:37 +0200

    refactoring

--- src/main/scala/it/twinsbrain/fpinscala/chapter3/List.scala
+++ src/main/scala/it/twinsbrain/fpinscala/chapter3/List.scala
@@ -7,13 +7,13 @@ case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
-  def sum(ints: List[Int]): Int = foldRight(ints, 0)(_ + _)
-
-  def product(ds: List[Double]): Double = foldRight(ds,1.0)(_ * _)
-
   def apply[A](as: A*): List[A] =
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
+
+  def sum(ints: List[Int]): Int = foldRight(ints, 0)(_ + _)
+
+  def product(ds: List[Double]): Double = foldRight(ds,1.0)(_ * _)
 
   def tail[A](aList: List[A]): List[A] = aList match {
     case Nil => Nil
