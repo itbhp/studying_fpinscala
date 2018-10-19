75a9f9d8fb27da1cf8e9cc72e5dcb2ccb64b6b30
Paolo Sciarra <itbhps@gmail.com>
2018-10-19 19:21:36 +0200

    refactored (read as wrote right abstraction for fold)

--- src/main/scala/it/twinsbrain/fpinscala/chapter3/Tree.scala
+++ src/main/scala/it/twinsbrain/fpinscala/chapter3/Tree.scala
@@ -7,20 +7,20 @@ case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
-  def fold[A, B](tree: Tree[A])(lMap: A => B)(bMap: (Tree[A], Tree[A]) => B): B = tree match {
-    case Leaf(v) => lMap(v)
-    case Branch(l, r) => bMap(l, r)
+  def fold[A, B](tree: Tree[A])(f: A => B)(g: (B, B) => B): B = tree match {
+    case Leaf(v) => f(v)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
   }
 
   def size[A](t: Tree[A]): Int =
-    fold(t)(_ => 1)((l, r) => 1 + size(l) + size(r))
+    fold(t)(_ => 1)((left_size, right_size) => 1 + left_size + right_size)
 
   def maximum(tree: Tree[Int]): Int =
-    fold(tree)(identity)((l, r) => maximum(l) max maximum(r))
+    fold(tree)(identity)((leftMax, rightMax) => leftMax max rightMax)
 
   def depth[A](tree: Tree[A]): Int =
-    fold(tree)(_ => 0)((r, l) => 1 + (depth(l) max depth(r)))
+    fold(tree)(_ => 0)((leftDepth, rightDepth) => 1 + (leftDepth max rightDepth))
 
   def map[A, B](tree: Tree[A])(f: A => B): Tree[B] =
-    fold(tree)(v => Leaf(f(v)): Tree[B])((l, r) => Branch(map(l)(f), map(r)(f)))
+    fold(tree)(v => Leaf(f(v)): Tree[B])((lTree, rTree) => Branch(lTree, rTree))
 }
