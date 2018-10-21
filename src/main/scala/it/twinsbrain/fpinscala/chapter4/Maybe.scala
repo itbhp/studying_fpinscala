cab3faa13896af0461aeefdd3e77cf9bcf7c2660
Paolo Sciarra <itbhps@gmail.com>
2018-10-21 11:46:44 +0200

    refactoring

--- src/main/scala/it/twinsbrain/fpinscala/chapter4/Maybe.scala
+++ src/main/scala/it/twinsbrain/fpinscala/chapter4/Maybe.scala
@@ -1,6 +1,6 @@
 package it.twinsbrain.fpinscala.chapter4
 
-sealed trait Maybe[+A]{
+sealed trait Maybe[+A] {
   def map[B](f: A => B): Maybe[B] = this match {
     case None => None
     case Just(v) => Just(f(v))
@@ -23,7 +23,7 @@ sealed trait Maybe[+A]{
   }
 
   def filter(p: A => Boolean): Maybe[A] = this match {
-    case Just(v) if(p(v)) => Just(v)
+    case Just(v) if (p(v)) => Just(v)
     case _ => None
   }
 }
@@ -32,16 +32,16 @@ case class Just[+A](get: A) extends Maybe[A]
 
 case object None extends Maybe[Nothing]
 
-object Maybe{
-  def lift[A,B](f: A => B) : Maybe[A] => Maybe[B] = (a: Maybe[A]) => a.map(f)
+object Maybe {
+  def lift[A, B](f: A => B): Maybe[A] => Maybe[B] = (a: Maybe[A]) => a.map(f)
 
-  def map2[A,B,C](a: Maybe[A], b: Maybe[B])(f: (A,B) => C) : Maybe[C] =
-    a.flatMap(valA => b.map(valB => f(valA,valB)))
+  def map2[A, B, C](a: Maybe[A], b: Maybe[B])(f: (A, B) => C): Maybe[C] =
+    a.flatMap(valA => b.map(valB => f(valA, valB)))
 
-  def sequence[A](xs: List[Maybe[A]]): Maybe[List[A]] =  traverse(xs)(identity)
+  def sequence[A](xs: List[Maybe[A]]): Maybe[List[A]] = traverse(xs)(identity)
 
   def traverse[A, B](xs: List[A])(f: A => Maybe[B]): Maybe[List[B]] = xs match {
     case Nil => Just(Nil)
-    case y::ys => f(y).flatMap(b => traverse(ys)(f).map(cs => b::cs))
+    case y :: ys => f(y).flatMap(b => traverse(ys)(f).map(cs => b :: cs))
   }
 }
