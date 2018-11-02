309ce0b1ddd1514d71db667fd225906e5d043378
Paolo Sciarra <itbhps@gmail.com>
2018-11-02 16:44:14 +0100

    implemented Par API using java.util.concurrent api

--- src/main/scala/it/twinsbrain/fpinscala/chapter7/Parallelism.scala
+++ src/main/scala/it/twinsbrain/fpinscala/chapter7/Parallelism.scala
@@ -1,20 +1,41 @@
 package it.twinsbrain.fpinscala.chapter7
 
-trait Par[A]
+import java.util.concurrent.{Callable, ExecutorService, Future, TimeUnit}
 
 object Par {
-  def fork[A](a: => Par[A]): Par[A] = ???
+  type Par[A] = ExecutorService => Future[A]
 
-  def unit[A](a: A): Par[A] = ???
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
 
-  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
 
-  def run[A](a: Par[A]): A = ???
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
 
-  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = ???
+    def get(timeout: Long, units: TimeUnit) = get
+
+    def isCancelled = false
+
+    def cancel(evenIfRunning: Boolean): Boolean = false
+  }
+
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = (es: ExecutorService) => {
+    val af = a(es)
+    val bf = b(es)
+    UnitFuture(f(af.get, bf.get))
+  }
+
+  def fork[A](a: => Par[A]): Par[A] =
+    es => es.submit(
+      new Callable[A] {
+        def call = a(es).get
+      }
+    )
 }
 
 object ParExample {
+
+  import Par._
 
   def sum(ints: IndexedSeq[Int]): Par[Int] = if (ints.length <= 1)
     Par.unit(ints.headOption getOrElse 0) else {
