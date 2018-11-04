e5cc62766a559da30dc24d1c586fb8ee44af2f90
Paolo Sciarra <itbhps@gmail.com>
2018-11-04 18:27:00 +0100

    tried to test parallel sum (with a fixed thread pool even with 6 numbers and 4 thread in 6 seconds and more it didn't finish)

---
+++ src/test/scala/it/twinsbrain/fpinscala/chapter7/ParExampleTest.scala
@@ -0,0 +1,17 @@
+package it.twinsbrain.fpinscala.chapter7
+
+import java.util.concurrent.{ExecutorService, Executors, TimeUnit}
+
+import it.twinsbrain.fpinscala.chapter7.Par.Par
+import org.scalatest._
+
+class ParExampleTest extends FunSuite with Matchers{
+  import ParExample._
+
+  test("sum"){
+    val parallelSum: Par[Int] = sum(Array(1, 2, 3))
+    val es = Executors.newFixedThreadPool(4)
+    parallelSum(es).get(6, TimeUnit.SECONDS)  shouldEqual 6
+  }
+
+}
