98b6c17125db0d036ad8d3e54eb6abdd57a0f253
Paolo Sciarra <itbhps@gmail.com>
2018-11-04 18:11:43 +0100

    modified map2 to respect timeout constraint on result

--- src/main/scala/it/twinsbrain/fpinscala/chapter7/Parallelism.scala
+++ src/main/scala/it/twinsbrain/fpinscala/chapter7/Parallelism.scala
@@ -9,6 +9,20 @@ object Par {
 
   def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
 
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
+    es => {
+      val (af, bf) = (a(es), b(es))
+      Map2Future(af, bf, f)
+    }
+
+  def fork[A](a: => Par[A]): Par[A] =
+    es => es.submit(
+      new Callable[A] {
+        def call = a(es).get
+      }
+    )
+
+
   private case class UnitFuture[A](get: A) extends Future[A] {
     def isDone = true
 
@@ -19,18 +33,36 @@ object Par {
     def cancel(evenIfRunning: Boolean): Boolean = false
   }
 
-  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = (es: ExecutorService) => {
-    val af = a(es)
-    val bf = b(es)
-    UnitFuture(f(af.get, bf.get))
+  case class Map2Future[A, B, C](a: Future[A], b: Future[B],
+                                 f: (A, B) => C) extends Future[C] {
+    @volatile var cache: Option[C] = None
+
+    def isDone = cache.isDefined
+
+    def isCancelled = a.isCancelled || b.isCancelled
+
+    def cancel(evenIfRunning: Boolean) =
+      a.cancel(evenIfRunning) || b.cancel(evenIfRunning)
+
+    def get = compute(Long.MaxValue)
+
+    def get(timeout: Long, units: TimeUnit): C =
+      compute(TimeUnit.NANOSECONDS.convert(timeout, units))
+
+    private def compute(timeoutInNanos: Long): C = cache match {
+      case Some(c) => c
+      case None =>
+        val start = System.nanoTime
+        val ar = a.get(timeoutInNanos, TimeUnit.NANOSECONDS)
+        val stop = System.nanoTime;
+        val aTime = stop - start
+        val br = b.get(timeoutInNanos - aTime, TimeUnit.NANOSECONDS)
+        val ret = f(ar, br)
+        cache = Some(ret)
+        ret
+    }
   }
 
-  def fork[A](a: => Par[A]): Par[A] =
-    es => es.submit(
-      new Callable[A] {
-        def call = a(es).get
-      }
-    )
 }
 
 object ParExample {
