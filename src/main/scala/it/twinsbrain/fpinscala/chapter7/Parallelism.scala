8b86e6e4438fd8d178bb635127bb50d78e06fc48
Paolo Sciarra <itbhps@gmail.com>
2018-11-04 21:24:16 +0100

    filtering list in parallel

--- src/main/scala/it/twinsbrain/fpinscala/chapter7/Parallelism.scala
+++ src/main/scala/it/twinsbrain/fpinscala/chapter7/Parallelism.scala
@@ -78,12 +78,20 @@ object ParExample {
 
   def asyncF[A, B](f: A => B): A => Par[B] = a => map(lazyUnit(a))(f)
 
-  def parMap[A, B](ps: List[A])(f: A => B): Par[List[B]] = sequence(ps.map(asyncF(f)))
+  def parMap[A, B](ps: List[A])(f: A => B): Par[List[B]] = fork {
+    sequence(ps.map(asyncF(f)))
+  }
 
   //    ps match {
   //      case Nil => lazyUnit(Nil)
   //      case x :: xs => map2(lazyUnit(f(x)), fork(parMap(xs)(f)))(_ :: _)
   //    }
+
+  def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] = {
+    val z: Par[List[A]] = lazyUnit(List())
+    as.foldRight(z)((e, acc) => map2(lazyUnit(e), acc)((e, l) => if (f(e)) e :: l else l))
+  }
+
 
   def sum(ints: IndexedSeq[Int]): Par[Int] = if (ints.length <= 1)
     Par.unit(ints.headOption getOrElse 0) else {
--- src/test/scala/it/twinsbrain/fpinscala/chapter7/ParExampleTest.scala
+++ src/test/scala/it/twinsbrain/fpinscala/chapter7/ParExampleTest.scala
@@ -5,23 +5,31 @@ import java.util.concurrent.{Executors, TimeUnit}
 import it.twinsbrain.fpinscala.chapter7.Par.Par
 import org.scalatest._
 
-class ParExampleTest extends FunSuite with Matchers{
+class ParExampleTest extends FunSuite with Matchers {
+
   import ParExample._
 
-  test("sum"){
+  test("sum") {
     val parallelSum: Par[Int] = sum(Array(1, 2, 3))
     val es = Executors.newFixedThreadPool(4)
-    parallelSum(es).get(6, TimeUnit.SECONDS)  shouldEqual 6
+    parallelSum(es).get(6, TimeUnit.SECONDS) shouldEqual 6
   }
 
-  test("run function asynchronously"){
+  test("run function asynchronously") {
     val es = Executors.newFixedThreadPool(1)
-    asyncF((n:Int) => n * n) (2)(es).get(5, TimeUnit.SECONDS) shouldEqual 4
+    asyncF((n: Int) => n * n)(2)(es).get(5, TimeUnit.SECONDS) shouldEqual 4
   }
 
-  test("parMap"){
+  test("parMap") {
     val es = Executors.newFixedThreadPool(5)
     val square: Int => Int = (x: Int) => x * x
-    parMap(List(1,2,3,4))(square)(es).get(5, TimeUnit.SECONDS) shouldEqual List(1,4,9,16)
+    parMap(List(1, 2, 3, 4))(square)(es).get(5, TimeUnit.SECONDS) shouldEqual List(1, 4, 9, 16)
+  }
+
+
+  test("parFilter") {
+    val es = Executors.newFixedThreadPool(5)
+    val even: Int => Boolean = (x: Int) => x % 2 == 0
+    parFilter(List(1, 2, 3, 4))(even)(es).get(5, TimeUnit.SECONDS) shouldEqual List(2, 4)
   }
 }
