4f15ed67ce5ef81f9c851ca7a575581ca9e22fba
Paolo Sciarra <itbhps@gmail.com>
2018-11-11 19:27:02 +0100

    [WIP] written a failing test (ignored) to prove that Par.run hang indefinitely on exception

--- src/test/scala/it/twinsbrain/fpinscala/chapter7/ParExampleTest.scala
+++ src/test/scala/it/twinsbrain/fpinscala/chapter7/ParExampleTest.scala
@@ -6,30 +6,36 @@ import it.twinsbrain.fpinscala.chapter7.Par.Par
 import org.scalatest._
 
 class ParExampleTest extends FunSuite with Matchers {
+  val es = Executors.newFixedThreadPool(1)
 
   import ParExample._
 
   test("sum") {
     val parallelSum: Par[Int] = sum(Array(1, 2, 3))
-    val es = Executors.newFixedThreadPool(4)
     Par.run(es)(parallelSum) shouldEqual 6
   }
 
   test("run function asynchronously") {
-    val es = Executors.newFixedThreadPool(1)
     Par.run(es)(asyncF((n: Int) => n * n)(2)) shouldEqual 4
   }
 
   test("parMap") {
-    val es = Executors.newFixedThreadPool(5)
     val square: Int => Int = (x: Int) => x * x
     Par.run(es)(parMap(List(1, 2, 3, 4))(square)) shouldEqual List(1, 4, 9, 16)
   }
 
 
   test("parFilter") {
-    val es = Executors.newFixedThreadPool(5)
     val even: Int => Boolean = (x: Int) => x % 2 == 0
     Par.run(es)(parFilter(List(1, 2, 3, 4))(even)) shouldEqual List(2, 4)
+  }
+
+  //FIXME
+  ignore("run on exception") {
+    val delayWithException: Int => Par[Int] = asyncF(x => {
+      throw new IllegalArgumentException
+      x
+    })
+    Par.run(es)(delayWithException(1)) shouldEqual 1
   }
 }
